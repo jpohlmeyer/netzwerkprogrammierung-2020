ba5462bbfdae93a9acf0d12c980d38b647cee512
jpohlmeyer <jpohlmeyer@techfak.uni-bielefeld.de>
2020-07-05 10:46:29 +0200

    Add some more service tests. See #3.

--- tests/tests_host.py
+++ tests/tests_host.py
@@ -14,16 +14,16 @@ class HostTest(unittest.TestCase):
     def test_simple_add_peer(self):
         host = MockHost("localhost", "7000", [], "masterscript.sh", "slavescript.sh")
         old_peers = host.peers.copy()
-        peer = Peer("localhost", "7001")
+        peer = Peer("peerhost", "7001")
         host.add_peer(peer)
         self.assertEqual(len(host.peers)-len(old_peers), 1, "Difference in number of peers should be 1")
-        self.assertEqual(host.peers[-1].host, "localhost", "Hostname of last peer should be localhost")
+        self.assertEqual(host.peers[-1].host, "peerhost", "Hostname of last peer should be peerhost")
         self.assertEqual(host.peers[-1].port, "7001", "Port of last peer should be 7001")
 
     def test_duplicate_add_peer(self):
         host = MockHost("localhost", "7000", [], "masterscript.sh", "slavescript.sh")
         old_peers = host.peers.copy()
-        peer = Peer("localhost", "7001")
+        peer = Peer("peerhost", "7001")
         host.add_peer(peer)
         host.add_peer(peer)
         self.assertEqual(len(host.peers)-len(old_peers), 1, "Difference in number of peers should be 1")
@@ -44,8 +44,8 @@ class HostTest(unittest.TestCase):
         self.assertEqual(len(host.peers), 0, "Number of peers should be 0")
 
     def test_start_with_not_started_peers(self):
-        peer1 = Peer("localhost", "7001")
-        peer2 = Peer("localhost", "7002")
+        peer1 = Peer("peer1host", "7001")
+        peer2 = Peer("peer2host", "7002")
         host = MockHost("localhost", "7000", [peer1, peer2], "masterscript.sh", "slavescript.sh")
         host.start()
         self.assertEqual(host.master.host, "localhost", "Master host should be localhost")
